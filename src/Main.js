972c1c45706e2c90fcf3bd472bde04cd00341045
Ian Pous <pousig@g.crookcountyschools.org>
2022-02-02 12:24:58 -0800

    Fixed Netlify bugs

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import beastData from './BeastData';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import beastData from './beastData';
 import FilterForm from "./FilterForm"
 
 class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            gallery: beastData
+        };
+    };
+
+    updateGallery = (filteredGallery) => {
+        this.setState({ gallery: filteredGallery });
+    }
     render() {
         return (
-        <Container>
-            <FilterForm /> 
-            <Row xs={1} md={2} lg={3} xl={4}>
-            {beastData.map((beast,idx)=> (
-             <HornedBeast key={idx} beastObj={beast}/>
-            ))}
-           </Row>
-        </Container>
+            <Container>
+                <FilterForm updateGallery={this.updateGallery}/>
+                <Row xs={1} md={2} lg={3} xl={4}>
+                    {this.state.gallery.map((beast, idx) => (
+                        <HornedBeast key={idx} beastObj={beast} />
+                    ))}
+                </Row>
+            </Container>
         );
     }
 }
